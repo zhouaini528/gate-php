cdb905e29462099c8828725805cda820c12f8bc3
zhouaini528 <465382251@qq.com>
2023-11-21 15:52:19 +0800

    add account test

---
+++ tests/margin/account.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @author lin <465382251@qq.com>
+ * */
+
+use Lin\Gate\GateMargin;
+
+require __DIR__ .'../../../vendor/autoload.php';
+
+include 'key_secret.php';
+
+$gate=new GateMargin($key,$secret);
+
+//You can set special needs
+$gate->setOptions([
+    //Set the request timeout to 60 seconds by default
+    'timeout'=>10,
+]);
+
+try {
+    $result=$gate->account()->get();
+    print_r($result);
+}catch (\Exception $e){
+    print_r(json_decode($e->getMessage(),true));
+}
+
+?>
---
+++ tests/spot/account.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @author lin <465382251@qq.com>
+ * */
+
+use Lin\Gate\GateSpot;
+
+require __DIR__ .'../../../vendor/autoload.php';
+
+include 'key_secret.php';
+
+$gate=new GateSpot($key,$secret);
+
+//You can set special needs
+$gate->setOptions([
+    //Set the request timeout to 60 seconds by default
+    'timeout'=>10,
+
+]);
+
+try {
+    $result=$gate->account()->get();
+    print_r($result);
+}catch (\Exception $e){
+    print_r(json_decode($e->getMessage(),true));
+}
+
+?>
